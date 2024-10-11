2a0858de6270e91e8e5a7215d3a0ebd60c14d116
caohoangthien <caohoangthienbkdn@gmail.com>
2024-10-11 14:16:58 +0700

    Update api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', function () {
-    return response()->json(['message' => 'This is response from api laravel.']);
+    return response()->json(['message' => 'This is response from api laravel 9.']);
 });
